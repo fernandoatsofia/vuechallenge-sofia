e4b567527fbb828ac6524a3834021346374491dc
Adrián Guerrero <a.guerrero.g870@gmail.com>
2019-04-01 19:12:24 -0600

    feat(*): add sorted item list

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Hola Vue</h1>
+  <div id="app" class="App">
+    <template v-if="loaded">
+      <div v-if="loadError">Hubo un error</div>
+      <item-list
+        v-else
+        :data="itemListData"
+      ></item-list>
+    </template>
   </div>
 </template>
+
 <script>
+import axios from 'axios';
+import ItemList from './ItemList.vue';
 
 export default {
   name: 'app',
+  components: {
+    ItemList,
+  },
+  data() {
+    return {
+      itemListData: null,
+      loadError: false,
+      loaded: false,
+    };
+  },
+  async mounted() {
+    await this.loadItemListData();
+  },
+  methods: {
+    async loadItemListData() {
+      try {
+        const resp = await axios.get('https://jsonplaceholder.typicode.com/users');
+        this.itemListData = resp.data;
+      } catch (err) {
+        this.loadError = true;
+      }
+      this.loaded = true;
+    },
+  },
 };
 </script>
 
 <style lang="scss">
+.App {
+  font-family:'Courier New', Courier, monospace;
+}
 </style>
---
+++ src/ItemList.vue
@@ -0,0 +1,49 @@
+<template>
+  <ul class="ItemList">
+    <li
+      v-for="item in sortedData"
+      :key="item.id"
+      class="ItemList-item"
+    >
+      <p>
+        <b>Name:</b> {{ item.name }}
+      </p>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'ItemList',
+  props: {
+    data: {
+      required: true,
+      type: Array,
+    },
+  },
+  computed: {
+    sortedData() {
+      const { data } = this;
+      return data.sort((a, b) => {
+        const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        // names must be equal
+        return 0;
+      });
+    },
+  },
+};
+</script>
+
+<style>
+.ItemList {
+  padding: 10px;
+  list-style-type: none;
+}
+</style>
